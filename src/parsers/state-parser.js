408f9b81a5b4442cfd4ed74ed004afa7fef7dfe4
Aleksei Ivanov <aleksei.ivanov@nordigy.ru>
2021-02-08 20:08:58 +0300

    fix 3+ word state name parsing

--- src/parsers/state-parser.js
+++ src/parsers/state-parser.js
@@ -1,11 +1,10 @@
-const { stat } = require("fs");
-
 function appendActionFunction(text, actionFunction, stateName) {
   const pattern = makeAppendPattern(stateName);
   return text.replace(pattern, (v) => `${v.trim()}\n${actionFunction}\n\n`);
 }
 
 function makeAppendPattern(stateName) {
+  // eslint-disable-next-line no-useless-escape
   const regexpString = `class \+${stateName}State\\s\*\{\\s\*\\n`;
   return new RegExp(regexpString, 'gm');
 }
--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -1,15 +1,22 @@
+/* eslint-disable import/no-unresolved */
 const assert = require('assert');
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 const vscode = require('vscode');
+const { describe, it } = require('mocha');
+const { extractStateNameFromUri } = require('../../extension');
+
 // const myExtension = require('../extension');
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  describe('extractStateNameFromUri(uri)', () => {
+    it('should parse correct long state name', () => {
+      const stateName = 'MyLongStateName';
+      const uri = '/some/folder/here/my-long-state-name.actions.ts';
+      assert.strictEqual(extractStateNameFromUri(uri), stateName);
+    });
+  });
 });
